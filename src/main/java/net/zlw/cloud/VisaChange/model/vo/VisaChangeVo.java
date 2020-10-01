f41c44e15332334057729c228c5f066cc814bab6
sjf2020 <shenjianfei0414@163.com>
2020-10-01 15:05:47 +0800

    20201001sjf签证变更列表Vo

---
+++ src/main/java/net/zlw/cloud/VisaChange/model/vo/VisaChangeVo.java
@@ -0,0 +1,50 @@
+package net.zlw.cloud.VisaChange.model.vo;
+
+import lombok.Data;
+
+
+@Data
+public class VisaChangeVo {
+    private int pageNum;
+    private int pageSize;
+    private String id;
+    private String ceaNum;
+    private String projectNum;
+    private String proportionContract;
+    private String district;
+    private String constructionUnit;
+    private String projectNature;
+    private String designCategory;
+    private String waterSupplyType;
+    private String customerName;
+    private String waterAddress;
+    private String proposer;
+    private String outsourcing;
+    private String nameOfCostUnit;
+    private String status;
+    private String createTime;
+    private String completionTime;
+    private String projectName;
+    private String createStartTime;
+    private String createEndTime;
+    private String keyword;
+
+    private String amountCost;
+    private String contractAmount;
+    private String contractAmountShang;
+    private String contractAmountXia;
+    private String baseProjectId;
+    private String loginUserId;
+    private String amountVisaChange;
+    private String amountVisaChangeAddXia;
+    private String amountVisaChangeAddShang;
+    private String proportionContractXia;
+    private String proportionContractShang;
+
+    private String amountVisaChangeXia;
+    private String amountVisaChangeShang;
+
+
+
+
+}
