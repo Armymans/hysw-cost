a19f3ce7ad5eec665f9294f3c02c5b855bea30f0
sjf2020 <shenjianfei0414@163.com>
2020-10-07 20:39:21 +0800

    20201007sjf

--- src/main/java/net/zlw/cloud/warningDetails/controller/WarningDetailsController.java
+++ src/main/java/net/zlw/cloud/warningDetails/controller/WarningDetailsController.java
@@ -1,19 +1,26 @@
 package net.zlw.cloud.warningDetails.controller;
 
+import net.tec.cloud.common.controller.BaseController;
+import net.tec.cloud.common.web.MediaTypes;
+import net.zlw.cloud.common.RestUtil;
+import net.zlw.cloud.warningDetails.model.WarningDetailAndAuditInfoVo;
 import net.zlw.cloud.warningDetails.model.WarningDetails;
 import net.zlw.cloud.warningDetails.service.WarningDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**  风险预警说明回显和编辑
- * Created by xulei on 2020/9/21.
+ * Created by xulei on 2020/9/21.  /hysw/cost/api/api/warningDetails/updateWarningDetails
+ *
  */
 @RestController
 @RequestMapping("/api/warningDetails")
-public class WarningDetailsController {
+public class WarningDetailsController extends BaseController {
 
      @Resource
      private WarningDetailsService warningDetailsService;
@@ -24,9 +31,10 @@ public class WarningDetailsController {
      * @param id
      * @return
      */
-   @GetMapping("/warningFindById")
-    public WarningDetails warningFindById(String id, String instructions, String userId , String companyId){
-       return warningDetailsService.warningFindById(id,instructions,userId,companyId);
+   @RequestMapping(value = "/warningFindById", method = {RequestMethod.GET}, produces = MediaTypes.JSON_UTF_8)
+    public Map<String,Object> warningFindById(String id, String instructions){
+       warningDetailsService.warningFindById(id,instructions,getLoginUser());
+       return RestUtil.success("保存成功");
    }
 
     /**
@@ -35,10 +43,17 @@ public class WarningDetailsController {
      * @param userId
      * @return
      */
-    @GetMapping("/detailById")
-    public WarningDetails detailById(String id,String userId) {
+    @RequestMapping(value = "/detailById", method = {RequestMethod.GET}, produces = MediaTypes.JSON_UTF_8)
+    public  Map<String,Object> detailById(String id,String userId) {
         //获取预警信息  获取审核信息
-        return warningDetailsService.detailById(id,userId);
+        WarningDetails warningDetails = warningDetailsService.detailById(id, userId);
+        return RestUtil.success(warningDetails);
+    }
+
+    @RequestMapping(value = "/updateWarningDetails", method = {RequestMethod.POST}, produces = MediaTypes.JSON_UTF_8)
+    public  Map<String,Object> updateWarningDetails(WarningDetailAndAuditInfoVo warningDetailAndAuditInfoVo) {
+        warningDetailsService.updateWarningDetails(warningDetailAndAuditInfoVo);
+        return RestUtil.success();
     }
 
 
--- src/main/java/net/zlw/cloud/warningDetails/mapper/WarningDetailsMapper.java
+++ src/main/java/net/zlw/cloud/warningDetails/mapper/WarningDetailsMapper.java
@@ -1,6 +1,7 @@
 package net.zlw.cloud.warningDetails.mapper;
 
 import net.zlw.cloud.warningDetails.model.WarningDetails;
+import org.apache.ibatis.annotations.Update;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
@@ -8,5 +9,14 @@ import tk.mybatis.mapper.common.Mapper;
  */
 @org.apache.ibatis.annotations.Mapper
 public interface WarningDetailsMapper extends Mapper<WarningDetails> {
+
+    @Update("update\n" +
+            "warning_details\n" +
+            "set\n" +
+            "update_time = #{updateTime},\n" +
+            "status = #{status},\n" +
+            "WHERE\n" +
+            "id = #{id}\n")
+    WarningDetails updateWarningDetails();
 
 }
