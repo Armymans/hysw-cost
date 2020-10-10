e899d207afcca1cf929e9c39fbcbc473fa56832d
lijing <lijing@123.com>
2020-10-10 09:06:34 +0800

    20201010lx

---
+++ src/main/java/net/zlw/cloud/snsEmailFile/mapper/EmailInfoMapper.java
@@ -0,0 +1,12 @@
+package net.zlw.cloud.snsEmailFile.mapper;
+
+import net.zlw.cloud.snsEmailFile.model.EmailInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author Armyman
+ * @Description 邮件mapper
+ * @Date 2020/10/9 16:39
+ **/
+public interface EmailInfoMapper extends Mapper<EmailInfo> {
+}
