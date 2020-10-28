5e6bb5a50ac4898912bc7f9e9177a398f6fe316f
lijing <lijing@123.com>
2020-10-28 14:56:33 +0800

    20201028lx

--- src/main/java/net/zlw/cloud/snsEmailFile/util/MailSender.java
+++ src/main/java/net/zlw/cloud/snsEmailFile/util/MailSender.java
@@ -159,7 +159,7 @@ public class MailSender {
 	 *            待发送的邮件信息
 	 */
 	public static boolean sendHtmlMail(MailInfo mailInfo) {
-		ExchangeMailUtil mailUtil = new ExchangeMailUtil("https://mail.huayanwater.com/EWS/exchange.asmx","zjglpt", "!rh@wN4m^#q");
+		ExchangeMailUtil mailUtil = new ExchangeMailUtil("https://mail.huayanwater.com/EWS/exchange.asmx","zao.jia", "CALL66me");
 		String toStr = mailInfo.getToAddress();
 		String[] to = new String[]{toStr};
 		try {
