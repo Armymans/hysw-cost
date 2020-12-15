b55288d7d50e113eebcbfe3f02ff2b18fb206caa
高世顺 <631465462@qq.com>
2020-12-16 01:25:20 +0800

    2020/10/10

--- src/main/java/net/zlw/cloud/warningDetails/service/WarningDetailsService.java
+++ src/main/java/net/zlw/cloud/warningDetails/service/WarningDetailsService.java
@@ -214,7 +214,7 @@ public class WarningDetailsService {
             if ("1".equals(warningDetailAndAuditInfoVo.getAuditResult())) {
                 Example example1 = new Example(WarningDetails.class);
                 Example.Criteria c = example1.createCriteria();
-                c.andEqualTo("baseId",warningDetails.getBaseId()+warningDetails.getRiskType());
+                c.andEqualTo("baseId",warningDetails.getBaseId());
                 c.andEqualTo("delFlag","0");
                 List<WarningDetails> warningDetails1 = warningDetailsMapper.selectByExample(example1);
                 for (WarningDetails details : warningDetails1) {
