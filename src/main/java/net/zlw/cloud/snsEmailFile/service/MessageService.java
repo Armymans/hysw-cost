1888a27fcd4873c3636b7b7cd3e07b0f28280f71
lijing <lijing@123.com>
2020-12-17 18:01:07 +0800

    20201217lx

--- src/main/java/net/zlw/cloud/snsEmailFile/service/MessageService.java
+++ src/main/java/net/zlw/cloud/snsEmailFile/service/MessageService.java
@@ -82,7 +82,7 @@ public class MessageService {
             messageNotification.setTitle(messageVo.getTitle());
             messageNotification.setDetails(messageVo.getDetails());
             messageNotification.setAcceptId(memberManage.getId());
-            messageNotification.setSender("华衍税务设计造价平台");
+            messageNotification.setSender("华衍水务设计造价平台");
             messageNotification.setCreateTime(data);
             messageNotification.setSubmitTime(data);
             messageNotification.setInformStatus("0");
