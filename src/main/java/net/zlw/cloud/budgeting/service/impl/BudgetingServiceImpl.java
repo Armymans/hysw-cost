d92ab29b3f81d82ae41a5118ae8b10aba7da5ce6
shenjianfei <shenjianfei0414@163.com>
2020-12-21 17:53:12 +0800

    20201212shenjianfei

--- src/main/java/net/zlw/cloud/budgeting/service/impl/BudgetingServiceImpl.java
+++ src/main/java/net/zlw/cloud/budgeting/service/impl/BudgetingServiceImpl.java
@@ -252,11 +252,7 @@ public class BudgetingServiceImpl implements BudgetingService {
         //控价编制
         VeryEstablishment veryEstablishment = new VeryEstablishment();
         veryEstablishment.setId(UUID.randomUUID().toString().replace("-",""));
-        if (budgetingVo.getBiddingPriceControl() != null && !"".equals(budgetingVo.getBiddingPriceControl())){
             veryEstablishment.setBiddingPriceControl(budgetingVo.getBiddingPriceControl());
-        }else {
-            throw new RuntimeException("请填入招标控制价");
-        }
         veryEstablishment.setVatAmount(budgetingVo.getVVatAmount());
         if (!"".equals(budgetingVo.getPricingTogether())){
             veryEstablishment.setPricingTogether(budgetingVo.getPricingTogether());
